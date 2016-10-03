502b8a9ca1a5cb5aa5596cacd7b31fdb936ec1d8
pike <pike.yoo@gmail.com>
2016-10-04 03:01:04 +0700

    add main link to announce

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,11 +5,14 @@
       @foreach ($announces as $announce)
       <div class="col-sm-6 col-md-4 col-lg-3">
           <div class="thumbnail">
-            <img src="{{URL::asset('uploads').'/'.$announce->img}}" alt="100x200">
-            <div class="caption">
-                <h3>{{$announce->title}}</h3>
-                <p>{{str_limit($announce->content, $limit = 50, $end = '...')}}</p>
-            </div>
+            <a href="{{route('showannounce').'/'.$announce->id}}">
+              <img src="{{URL::asset('uploads').'/'.$announce->img}}" alt="100x200">
+              <div class="caption">
+                  <h3>{{$announce->title}}</h3>
+                  <p>{{str_limit($announce->content, $limit = 50, $end = '...')}}</p>
+              </div>
+            </a>
+
           </div>
       </div>
       @endforeach
